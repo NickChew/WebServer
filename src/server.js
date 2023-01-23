eb6fed443d78f0c67545f27fcea8ecdcfb193084
NickChew <118730370+NickChew@users.noreply.github.com>
2023-01-23 14:13:37 +0000

    some css added

--- src/server.js
+++ src/server.js
@@ -10,3 +10,5 @@ app.use("/static", express.static("public"));
 app.listen(5001, () => {console.log("Listening on Port 5001")})
 //app.listen starts up express and tells it which port to listen in on. 
 //Also you can run a start up function which in this case just displays the message listening on port 5001
+
+// export default app;
